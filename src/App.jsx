bcae88d400f4f27f629931aaf37e36096cca1dc1
Sasan <sasanfarhadi@gmail.com>
2024-09-04 21:34:39 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import img from "./assets/1.png"
+function App() {
+  return (
+    <>
+      <h1>Book App Starting files</h1>
+      <img src={img} alt="" />
+    </>
+  );
+}
+export default App;
