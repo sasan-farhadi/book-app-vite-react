e52d122498c7bffd27897e1b042f94c9c583300a
Sasan <sasanfarhadi@gmail.com>
2024-09-04 21:54:22 +0330

    constant handl

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import img from "./assets/1.png"
+import { books } from './constants/mockData'
+
 function App() {
   return (
     <>
-      <h1>Book App Starting files</h1>
-      <img src={img} alt="" />
+      <h1></h1>
+      {
+        books.map(x => {
+          return (
+            <p>{x.title}</p>
+          )
+        })
+      }
     </>
   );
 }
--- src/constants/mockData.js
+++ src/constants/mockData.js
@@ -1,9 +1,20 @@
+import img1 from "../assets/1.png"
+import img2 from "../assets/2.png"
+import img3 from "../assets/3.png"
+import img4 from "../assets/4.png"
+import img5 from "../assets/5.png"
+import img6 from "../assets/6.png"
+import img7 from "../assets/7.png"
+import img8 from "../assets/8.png"
+import img9 from "../assets/9.png"
+import img10 from "../assets/10.png"
+
 const books = [
   {
     id: "001",
     author: "Chinua Achebe",
     country: "Nigeria",
-    image: "1.png",
+    image: img1,
     language: "English",
     link: "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
     pages: 209,
@@ -14,7 +25,7 @@ const books = [
     id: "002",
     author: "Hans Christian Andersen",
     country: "Denmark",
-    image: "2.png",
+    image: img2,
     language: "Danish",
     link: "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
     pages: 784,
@@ -25,7 +36,7 @@ const books = [
     id: "003",
     author: "Dante Alighieri",
     country: "Italy",
-    image: "3.png",
+    image: img3,
     language: "Italian",
     link: "https://en.wikipedia.org/wiki/Divine_Comedy\n",
     pages: 928,
@@ -36,7 +47,7 @@ const books = [
     id: "004",
     author: "Maureen Gallery Kovacs",
     country: "Sumer and Akkadian Empire",
-    image: "4.png",
+    image: img4,
     language: "Akkadian",
     link: "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
     pages: 160,
@@ -47,7 +58,7 @@ const books = [
     id: "005",
     author: "Graham Ricardo",
     country: "Achaemenid Empire",
-    image: "5.png",
+    image: img5,
     language: "Hebrew",
     link: "https://en.wikipedia.org/wiki/Book_of_Job\n",
     pages: 176,
@@ -58,7 +69,7 @@ const books = [
     id: "006",
     author: "Hanan Al-Shaykh",
     country: "India/Iran/Iraq/Egypt/Tajikistan",
-    image: "6.png",
+    image: img6,
     language: "Arabic",
     link: "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
     pages: 288,
@@ -69,7 +80,7 @@ const books = [
     id: "007",
     author: "Robert Cook",
     country: "Iceland",
-    image: "7.png",
+    image: img7,
     language: "English",
     link: "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga\n",
     pages: 384,
@@ -80,7 +91,7 @@ const books = [
     id: "008",
     author: "Jane Austen",
     country: "United Kingdom",
-    image: "8.png",
+    image: img8,
     language: "English",
     link: "https://en.wikipedia.org/wiki/Pride_and_Prejudice\n",
     pages: 226,
@@ -91,7 +102,7 @@ const books = [
     id: "009",
     author: "Honor\u00e9 de Balzac",
     country: "France",
-    image: "9.png",
+    image: img9,
     language: "French",
     link: "https://en.wikipedia.org/wiki/Le_P%C3%A8re_Goriot\n",
     pages: 443,
@@ -102,7 +113,7 @@ const books = [
     id: "010",
     author: "Samuel Beckett",
     country: "Republic of Ireland",
-    image: "10.png",
+    image: img10,
     language: "French",
     link: "https://en.wikipedia.org/wiki/Molloy_(novel)\n",
     pages: 256,
